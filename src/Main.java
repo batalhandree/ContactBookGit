ecab16314e13e17eac1352f2ae66f263ecdeb003
Andre <a.batalha@gmail.campus.fct.unl.pt>
2026-09-18 18:20:43 +0100

    mensagens na main

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,8 @@ public class Main {
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
     public static final String QUIT           = "Q";
+    public static final String GET_NUMBER    = "GN";
+    public static final String EQUAL_PHONE    = "EP";
 
     //Constantes que definem as mensagens para o utilizador
     public static final String CONTACT_EXISTS = "contactBook.Contact already exists.";
@@ -24,6 +26,9 @@ public class Main {
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
+    public static final String PHONE_NOT_EXIST = "Phone number does not exist.";
+    public static final String SHARE_PHONE = "There are contacts that share phone numbers.";
+    public static final String NOT_SHARE_PHONE = "All contacts have different phone numbers";
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
